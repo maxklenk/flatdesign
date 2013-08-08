84bd443564615ed00d50c018432cd3695a253b5f
Max Klenk <max@klenk.biz>
2013-08-08 20:06:30 +0200

    basic header image support added

--- functions.php
+++ functions.php
@@ -171,5 +171,89 @@ function bones_wpsearch($form) {
 	return $form;
 } // don't remove this bracket!
 
+/************* HEADER IMAGE *********************/
 
+/**
+ * Tell WordPress to run header_setup() when the 'after_setup_theme' hook is run.
+ */
+add_action( 'after_setup_theme', 'header_setup' );
+
+if ( ! function_exists( 'header_setup' ) ):
+/**
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * @uses register_default_headers() To register the default custom header images provided with the theme.
+ * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
+ *
+ */
+function header_setup() {
+
+	// This theme uses Featured Images (also known as post thumbnails) for per-post/per-page Custom Header images
+	add_theme_support( 'post-thumbnails' );
+
+	// Add support for custom headers.
+	$custom_header_support = array(
+		// The default header text color.
+		'default-text-color' => '000',
+		// The height and width of our custom header.
+		'width' => apply_filters( 'flatdesign_header_image_width', 1140 ),
+		'height' => apply_filters( 'flatdesign_header_image_height', 330 ),
+		// Support flexible heights.
+		'flex-height' => true,
+	);
+
+	add_theme_support( 'custom-header', $custom_header_support );
+
+	// We'll be using post thumbnails for custom header images on posts and pages.
+	// We want them to be the size of the header image that we just defined
+	// Larger images will be auto-cropped to fit, smaller ones will be ignored. See header.php.
+	set_post_thumbnail_size( $custom_header_support['width'], $custom_header_support['height'], true );
+
+	// Add custom image sizes.
+	// Used for large feature (header) images.
+	add_image_size( 'large-feature', $custom_header_support['width'], $custom_header_support['height'], true );
+	// Used for featured posts if a large-feature doesn't exist.
+	add_image_size( 'small-feature', 500, 300 );
+
+	// Default custom headers packaged with the theme. %s is a placeholder for the theme template directory URI.
+	register_default_headers( array(
+		'sky' => array(
+			'url' => '%s/library/images/headers/sky-1140-330.jpg',
+			'thumbnail_url' => '%s/library/images/headers/sky-thumbnail.jpg',
+			/* translators: header image description */
+			'description' => __( 'Sky', 'flatdesign' )
+		)
+	) );
+}
+endif; // twentyeleven_setup
+
+function get_flatdesign_header_image() {
+
+		// Check to see if the header image has been removed
+		$header_image = get_header_image();
+		if ( $header_image ) :
+			// We need to figure out what the minimum width should be for our featured image.
+			// This result would be the suggested width if the theme were to implement flexible widths.
+			$header_image_width = get_theme_support( 'custom-header', 'width' );
+	?>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<?php
+			// The header image
+			// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+			if ( is_singular() && has_post_thumbnail( $post->ID ) &&
+					( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
+					$image[1] >= $header_image_width ) :
+				// Houston, we have a new header image!
+				echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+			else :
+				$header_image_width  = get_custom_header()->width;
+				$header_image_height = get_custom_header()->height;
+		?>
+			<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
+		<?php endif; // end check for featured image or standard header ?>
+	</a>
+<?php endif; // end check for removed header image 
+}
 ?>
--- header.php
+++ header.php
@@ -50,9 +50,7 @@
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
 					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 
-					<!-- if you'd like to use the site description you can un-comment it below -->
-					<?php // bloginfo('description'); ?>
-
+					<?php echo get_flatdesign_header_image(); ?>
 
 					<nav role="navigation">
 						<?php bones_main_nav(); ?>
