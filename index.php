2af3732e0c455d12ec05dbe5220ca71f2aadb2ed
Max Klenk <max@klenk.biz>
2013-08-22 14:44:18 +0200

    post layout moved in seperat file

--- index.php
+++ index.php
@@ -8,32 +8,7 @@
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									
-									<time class="updated article-date" datetime="<?=get_the_time('Y-m-j')?>" pubdate>
-										<span class="article-date-day"><?=get_the_time('j')?></span>
-										<span class="article-date-month"><?=get_the_time('M')?></span>
-										<span class="article-date-year"><?=get_the_time('Y')?></span>
-									</time>
-										
-								</header> <!-- end article header -->
-
-								<section class="entry-content clearfix">
-									<?php the_content(); ?>
-								</section> <!-- end article section -->
-
-								<footer class="article-footer">
-									<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?></p>
-									<p class="categories"><span class="categories-title"> <?=__('Categories:', 'bonestheme') ?> </span> <?=get_the_category_list(', ') ?></p>
-								</footer> <!-- end article footer -->
-
-								<?php // comments_template(); // uncomment if you want to use them ?>
-
-							</article> <!-- end article -->
+								<?php get_template_part('post'); ?>
 
 							<?php endwhile; ?>
 
--- single.php
+++ single.php
@@ -13,9 +13,11 @@
 								<header class="article-header">
 
 									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-									<p class="byline vcard"><?php
-										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
+									<time class="updated article-date" datetime="<?=get_the_time('Y-m-j')?>" pubdate>
+										<span class="article-date-day"><?=get_the_time('j')?></span>
+										<span class="article-date-month"><?=get_the_time('M')?></span>
+										<span class="article-date-year"><?=get_the_time('Y')?></span>
+									</time>
 
 								</header> <!-- end article header -->
 
@@ -25,7 +27,7 @@
 
 								<footer class="article-footer">
 									<?php the_tags('<p class="tags"><span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', '</p>'); ?>
-
+									<p class="categories"><span class="categories-title"> <?=__('Categories:', 'bonestheme') ?> </span> <?=get_the_category_list(', ') ?></p>
 								</footer> <!-- end article footer -->
 
 								<?php comments_template(); ?>
