ce1fe19c0beea0fdaebe834b8c4e9ed3d1c42138
Max Klenk <max@klenk.biz>
2013-09-04 13:48:09 +0200

    layout footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 				<div id="inner-footer" class="wrap clearfix">
 
-					<div id="footer-widgets">
+					<div id="footer-widgets" class="ninecol first">
 
 						<?php if ( is_active_sidebar( 'footer' ) ) : ?>
 
@@ -12,7 +12,7 @@
 
 					</div> <!-- end #footer-widgets -->
 
-					<div id="footer-links">
+					<div id="footer-links" class="threecol last">
 
 						<nav role="navigation">
 							<?php bones_footer_links(); ?>
